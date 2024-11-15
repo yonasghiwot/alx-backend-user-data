1efde9bd451c3db70ca7ae690151b37d6f910936
Yonas Gebrehiwot <yonihabeshaw@gmail.com>
2024-11-15 13:20:48 +0300

    session_auth.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" initiolizes the api and loads file
+""" initalization doc
 """
 from flask import Blueprint
 
@@ -9,3 +9,5 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 
 User.load_from_file()
+
+from api.v1.views.session_auth import *
